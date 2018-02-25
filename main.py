8167d229269433998d1a6cd3b02d8a43baa3f2c4
StenG <trieutienv@gmail.com>
2018-02-25 22:53:04 +0700

    Fix again

--- main.py
+++ main.py
@@ -20,41 +20,44 @@ def start_operation():
   if box.get() == 'DA':
     txtstart ='service dadaemon start'
     cmp_clipboard(txtstart)
+    testButton.config(text=txtstart)
     move_mouse()
   if box.get() == 'DC':
     txtstart ='service dcmd start'
     cmp_clipboard(txtstart)
+    testButton.config(text=txtstart)
     move_mouse()
-  testButton.config(text=txtstart)
 def stop_operation():
   if box.get() == 'DA':
     txtstop ='service dadaemon stop'
     cmp_clipboard(txtstop)
+    testButton.config(text=txtstart)
     move_mouse()
   if box.get() == 'DC':
     txtstop ='service dcmd stop'
     cmp_clipboard(txtstop)
+    testButton.config(text=txtstart)
     move_mouse()
-  testButton.config(text=txtstop)
 def status_operation():
   if box.get() == 'DA':
     txtstatus ='service dadaemon status'
     cmp_clipboard(txtstatus)
+    testButton.config(text=txtstart)
     move_mouse()
   if box.get() == 'DC':
     txtstatus ='service dcmd status'
     cmp_clipboard(txtstatus)
+    testButton.config(text=txtstart)
     move_mouse()
   if box.get() == 'Vertica':
     txtstatus = 'admintools -t db_status -s UP'
+    testButton.config(text=txtstart)
     move_mouse()
-  testButton.config(text=txtstatus)
 def cmp_clipboard(txt):
   r.withdraw()
   r.clipboard_clear()
   r.clipboard_append(txt)
   r.update()
-  r.destroy()
 def move_mouse():
   pyautogui.moveTo(1000, 500)
   pyautogui.rightClick()
