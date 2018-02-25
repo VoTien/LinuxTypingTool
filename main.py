6a2490f62960efcf8b1b4af6efff4617c378eef0
StenG <trieutienv@gmail.com>
2018-02-25 22:18:28 +0700

    Final step. Waiting for build program

--- main.py
+++ main.py
@@ -1,42 +1,66 @@
 from Tkinter import *
 import ttk
-
+import pyautogui
 import os
-
+import pyautogui
 
 #--Main Windows
 window = Tk()
 window.title("LAZYNESS...")
 window.geometry("200x300")
-
+#Create Tkinter clipboard
+r = Tk()
+r.withdraw()
+r.clipboard_clear()
+#TESTGLOBAL
 testButton =Label(window,text="")
 testButton.grid(column=0, row=5)
 
 #--Functions
 def start_operation():
   if box.get() == 'DA':
-    textstart ='service dadaemon start'
+    txtstart ='service dadaemon start'
+    move_mouse()
   if box.get() == 'DC':
-    textstart ='service dcmd start'
-  testButton.config(text=textstart)
-  r = Tk()
-  r.withdraw()
-  r.clipboard_clear()
-  r.clipboard_append(textstart)
-  r.update()
-  r.destroy()
+    txtstart ='service dcmd start'
+    move_mouse()
+  else:
+    testButton.config(text='There nothing OID')
+  testButton.config(text=txtstart)
+  cmp_clipboard(txtstart)
 def stop_operation():
   if box.get() == 'DA':
-    textstart ='service dadaemon stop'
+    txtstop ='service dadaemon stop'
+    move_mouse()
   if box.get() == 'DC':
-    textstart ='service dcmd stop'
-  testButton.config(text=textstart)
+    txtstop ='service dcmd stop'
+    move_mouse()
+  else:
+    testButton.config(text='There nothing OID')
+  testButton.config(text=txtstop)
+  cmp_clipboard(txtstop)
 def status_operation():
   if box.get() == 'DA':
-    textstart ='service dadaemon status'
+    txtstatus ='service dadaemon status'
+    move_mouse()
   if box.get() == 'DC':
-    textstart ='service dcmd status'
-  testButton.config(text=textstart)
+    txtstatus ='service dcmd status'
+    move_mouse()
+  if box.get() == 'Vertica':
+    txtstatus = 'admintools -t db_status -s UP'
+    move_mouse()
+  testButton.config(text=txtstatus)
+  cmp_clipboard(txtstatus)
+def cmp_clipboard(txt):
+  r.withdraw()
+  r.clipboard_clear()
+  r.clipboard_append(txt)
+  r.update()
+  r.destroy()
+def move_mouse():
+  pyautogui.moveTo(1000, 500)
+  pyautogui.rightClick()
+  pyautogui.press('enter')
 
 #--Labels
 title = Label(window,text="Please select type of Machine")
@@ -45,9 +69,10 @@ title.grid(column=0,row=0)
 #Combobox
 value = StringVar()
 box = ttk.Combobox(window, textvariable=value, state='readonly')
-box['values'] = ('DA','DC')
+box['values'] = ('DC','DA','Vertica')
 box.current(0)
 box.grid(column=0, row=1)
+testButton.config(text=box.get())
 
 #--Buttons
 buttonStart = Button(text="Start",command=start_operation)
